91f50d972f1d11a07e278e520e8eeef34dbad0f7
Cornelius Behrend <cornelius@staffbase.com>
2021-05-31 17:07:36 +0200

    chore: fix merge error

--- samples/weather-forecast/src/components/Card/index.tsx
+++ samples/weather-forecast/src/components/Card/index.tsx
@@ -20,18 +20,6 @@ export interface CardProperties {
 }
 
 export const Card: FunctionComponent<CardProperties> = (props) => {
-  const cardStyle: CSS.Properties = {
-    display: "block",
-    color: "white",
-    boxShadow: "0 0 8px 0 rgba(0, 0, 0, 0.15)",
-    borderRadius: "0.5rem",
-    height: "14rem",
-    minWidth: "25rem",
-    fontFamily: "Open Sans",
-    fontStyle: "normal",
-    position: "relative",
-    backgroundColor: props.color,
-  };
 
     const limitedDeviceSize = useMediaQuery('(max-width: 25rem)');
 
@@ -56,5 +44,4 @@ export const Card: FunctionComponent<CardProperties> = (props) => {
             {props.children}
         </div>
     );
-}
 }
