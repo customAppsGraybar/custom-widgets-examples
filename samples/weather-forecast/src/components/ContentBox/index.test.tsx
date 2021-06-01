1b5c8e945c9a52cd7ec9a8d71d1d406aae5f71ca
Cornelius Behrend <cornelius@staffbase.com>
2021-06-01 16:11:04 +0200

    chore: fix tests

--- samples/weather-forecast/src/components/ContentBox/index.test.tsx
+++ samples/weather-forecast/src/components/ContentBox/index.test.tsx
@@ -13,6 +13,7 @@
 
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+
 import { ContentBox } from ".";
 
 describe("ContentBox", () => {
--- samples/weather-forecast/test/jest-setup.js
+++ samples/weather-forecast/test/jest-setup.js
@@ -1,1 +1,4 @@
 import '@testing-library/jest-dom'
+import MatchMediaMock from 'jest-matchmedia-mock';
+
+new MatchMediaMock()
